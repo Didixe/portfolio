c20ebc9cb0224c278e52ef01bece218ccf75f7db
Adeline Gilet <Adeline.gilet@gmail.com>
2024-05-27 16:35:23 +0200

    maileur

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -5,29 +5,38 @@ namespace App\Service;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\component\DependencyInjection\Attribute\Autowire;
 use Twig\Environment;
 
 class EmailService
 {
-    private MailerInterface $mailer;
-    private $twig;
-    private $urlGenerator;
+//    #[Autowire('%admin_email%')] private string $adminEmail;
+//    private MailerInterface $mailer;
+//    private $twig;
+//    private $urlGenerator;
 
-    public function __construct(MailerInterface $mailer, Environment $twig, UrlGeneratorInterface $urlGenerator)
+//    public function __construct(MailerInterface $mailer, Environment $twig, UrlGeneratorInterface $urlGenerator)
+    public function __construct(
+        #[Autowire('%admin_email%')] private string $adminEmail,
+        private MailerInterface $mailer,
+    )
     {
-        $this->mailer = $mailer;
-        $this->twig = $twig;
-        $this->urlGenerator = $urlGenerator;
+//        $this->mailer = $mailer;
+//        $this->twig = $twig;
+//        $this->urlGenerator = $urlGenerator;
     }
 
     public function sendContactEmail(array $formData): void
     {
         // Création de l'e-mail
         $email = (new Email())
-            ->from($formData['Email'])
-            ->to('adeline.gilet33@gmail.com')
+//            ->from($formData['Email'])
+            ->from($this->adminEmail)
+//            ->to('adeline.gilet33@gmail.com')
+            ->to($this->adminEmail)
             ->subject('Nouveau message de ' . $formData['Name'])
             ->text('Nom: ' . $formData['Name'] . "\n" .
+                'Email: ' . $formData['Email'] . "\n" .
                 'Société: ' . $formData['company'] . "\n" .
                 'Téléphone: ' . $formData['phone'] . "\n" .
                 'Message: ' . $formData['message']);
